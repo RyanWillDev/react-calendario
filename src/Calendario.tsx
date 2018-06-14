c997083a93730a048b562dea265ccd16abba02e9
Ryan Will <ryanwilldev@gmail.com>
2018-06-13 20:06:58 -0500

    fixes typescript import issues

--- src/Calendario.tsx
+++ src/Calendario.tsx
@@ -72,3 +72,13 @@ export default class Calendario extends React.Component<
     });
   }
 }
+
+/*
+ * Fix importing in typescript after rollup compilation
+ * https://github.com/rollup/rollup/issues/1156
+ * https://github.com/Microsoft/TypeScript/issues/13017#issuecomment-268657860
+ *
+ * Thanks @kentcdodds!
+ */
+
+(Calendario as any).default = Calendario;
